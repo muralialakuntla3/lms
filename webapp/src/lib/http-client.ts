7a4645a0fd069557a84b35c23eab8a6f6b773994
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2024-01-04 11:27:28 +0530

    Update http-client.ts

--- webapp/src/lib/http-client.ts
+++ webapp/src/lib/http-client.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// Define a function to safely access the environment variables from window
+function getRuntimeConfig() {
+  return window.env && window.env.VITE_API_URL ? window.env.VITE_API_URL : 'http://localhost:8080/api';
+}
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: getRuntimeConfig(),
 });
 
 export default axiosInstance;
